0b51bec485191002d3421a4a9a3f35738554fcf2
superjae0625 <68308300+superjae0625@users.noreply.github.com>
2023-02-02 20:12:41 -0700

    Add files via upload

---
+++ cmdline.hpp
@@ -0,0 +1,14 @@
+#ifndef cmdline_hpp
+#define cmdline_hpp
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+void use_arguments(int argc, const char * argv[]);
+
+#endif /* cmdline_hpp */
