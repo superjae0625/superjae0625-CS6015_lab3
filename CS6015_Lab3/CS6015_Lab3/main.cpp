cbeae194a1b8c060647c245b9604d8c92868fd52
Avishek Choudhury <u1428226@umail.utah.edu>
2023-02-02 20:13:27 -0700

    Adding Code coverage report

---
+++ CS6015_Lab3/CS6015_Lab3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  CS6015_Lab3
+//
+//  Created by Avishek Choudhury on 2/2/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
